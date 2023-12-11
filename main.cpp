61d6acdb6ffc24e31a99369ad78e3d16476703f3
hrizwan3 <hrizwan5472013@gmail.com>
2023-12-11 10:56:52 -0500

    game and solver complete

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
  * ****************************/
 
 int main(int argc, char* argv[]) {
-    cout << "Hello! Welcome to Fraudulent Sudoku (with a solver)" << endl;
+    cout << endl <<"Hello! Welcome to Fraudulent Sudoku (with a solver)" << endl;
     string choices[] = {"Load last game", "Create new game"};
     string boards[] = {"easy1", "easy2", "easy3", "grid1", "grid2"};
 
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]) {
     cout << "You selected: " << choices[val - 1] << endl;
 
     Sudoku sudoku;
-    if (val == 0) {
+    if (val == 1) {
         if (!loadSudoku(sudoku, "saves/save.txt")) {
             cout << "Game not found, loading a preset" << endl;
             loadSudoku(sudoku, "preset-games/easy1.txt");
@@ -35,20 +35,24 @@ int main(int argc, char* argv[]) {
         loadSudoku(sudoku, ("preset-games/" + boards[val - 1] + ".txt").c_str());
     }
 
-    bool playing{true};
+    bool player{true};
 
     while (!sudoku.checkSolved()) {
         sudoku.printBoard();
-        cout << "Please enter a row, column, and value (separated by spaces), or type \"-1 -1 -1\" to stop:" << endl;
+        cout << "Please enter a row, column, and value (separated by spaces), or type \"-1 -1 -1\" to stop, or \"-2 -2 -2\" to solve: ";
         int row, col, value;
         cin >> row >> col >> value;
         if (row == -1 && col == -1 && value == -1) {
-            cout << "Would you like to save your game? (y/n)" << endl;
+            cout << "Would you like to save your game? (y/n) ";
             char save;
             cin >> save;
             if (save == 'y') {
                 saveSudoku(sudoku, "saves/save.txt");
             }
+            break;
+        } else if (row == -2 && col == -2 && value == -2) { // doesn't work if you modify the board
+            sudoku.solve();
+            player = false;
             break;
         } else if (sudoku.isValidMove(row - 1, col - 1, value)) {
             sudoku.setCell(row - 1, col - 1, value);
@@ -57,8 +61,11 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (sudoku.checkSolved()) {
+    if (player && sudoku.checkSolved()) {
         cout << "Congratulations! You solved the puzzle!" << endl;
+    } else if (sudoku.checkSolved()) {
+        cout << "Here's the answer:" << endl;
+        sudoku.printBoard();
     }
     return 0;
 }
